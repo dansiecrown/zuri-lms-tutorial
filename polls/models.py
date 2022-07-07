a1641ac8619a7888a708a2102f90414f2276e3a7
dansiecrown <dansiecrown@gmail.com>
2022-07-07 11:07:49 +0100

    Polls: Added question description and migrations

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=225)
+    question_description = models.CharField(max_length=225, default="Basic migrations")
     pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question_text
 
 class Choices(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_answer = models.CharField(max_length=20)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_answer
